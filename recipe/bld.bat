c36a4f6a188aab67c1dd36216fcb49e012df7014
Daniel Ching <carterbox@users.noreply.github.com>
2024-02-29 21:19:15 -0600

    BUG: Use prefix placeholder in pkgconfig files

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -15,3 +15,12 @@ if errorlevel 1 exit 1
 
 meson install -C builddir --no-rebuild
 if errorlevel 1 exit 1
+
+setlocal EnableExtensions ENABLEDELAYEDEXPANSION
+
+for %%f in ( "%SRC_DIR%\stage\lib\pkgconfig\*.pc" ) do (
+    sed -i.bak "s,prefix=.*,prefix=/opt/anaconda1anaconda2anaconda3/Library,g" %%f
+    del %%f.bak
+)
+
+endlocal
